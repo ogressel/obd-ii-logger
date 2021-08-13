74f6e4cbb22dc70ca992892951165fdda9f25cae
Oliver Gressel <gressel@nbi.ku.dk>
2021-08-13 15:21:09 +0200

    add copyright notice.

--- obd-ii-logger.py
+++ obd-ii-logger.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 #
+#  ---=== simple data logging for OBD-II sensor PIDs ===---
+#
+#  This file is part of https://github.com/ogressel/obd-ii-logger
+#  Copyright (c) 2021 by Oliver Gressel <ogressel@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
 import sys
